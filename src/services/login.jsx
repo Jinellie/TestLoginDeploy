feb8d23245ca1e004c8140c2f8d4dc5a39980dcb
Jinellie <89491839+Jinellie@users.noreply.github.com>
2024-05-13 18:24:14 -0600

    test

--- src/services/login.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = 'http://autenticacion-global-dreamlab.azurewebsites.net/api/user/login'
-
-const login = async credentials => {
-  const response = await axios.post(baseUrl, credentials);
-  return response.data
-}
-
-export default { login }
